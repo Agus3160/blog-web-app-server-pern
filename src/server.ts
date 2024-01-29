c13bd338c1a4360a66844cfa6244c2ce0262dba5
Agus3160 <ampharos321@hotmail.com>
2024-01-29 17:00:52 -0300

    Ready to deploy

--- src/server.ts
+++ src/server.ts
@@ -10,7 +10,7 @@ dotenv.config()
 
 const server = express()
 
-server.use(cors({origin: 'http://localhost:5173', credentials: true}))
+server.use(cors({origin: 'https://web-app-blog-pern.onrender.com', credentials: true}))
 server.use(express.json())
 server.use(express.urlencoded({ extended: true }))
 server.use(cookieParser())
@@ -21,4 +21,4 @@ server.use('/user', user)
 
 server.use(errorHandler)
 
-server.listen(3000, () => console.log('server running on port 3000'))
+server.listen(3000, () => console.log('Server running on port 3000'))
